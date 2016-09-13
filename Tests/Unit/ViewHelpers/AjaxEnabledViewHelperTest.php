122353fd2e7fb287d1dcb27c902dedf2db729663
Georg Ringer <georg.ringer@gmail.com>
2016-09-13 12:09:28 +0200

    [TASK] Change requirements

---
+++ Tests/Unit/ViewHelpers/AjaxEnabledViewHelperTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Sup7even\Mailchimp\Tests\Unit\ViewHelpers;
+
+use Sup7even\Mailchimp\ViewHelpers\AjaxEnabledViewHelper;
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+
+class AjaxEnabledViewHelperTest extends UnitTestCase
+{
+
+    /**
+     * @test
+     * @dataProvider ajaxEnabledWorksDataProvider
+     */
+    public function ajaxEnabledWorks($setting, $extIsLoaded, $expeted)
+    {
+        
+        $viewHelper = $this->getAccessibleMock(AjaxEnabledViewHelper::class, array('renderChildren'), array(), '', false);
+        $viewHelper->method('renderChildren')->willReturn($code);
+
+        $pageRender = $this->prophesize(PageRenderer::class);
+        $pageRender->addFooterData($code)->shouldBeCalled();
+
+        $viewHelper->_set('pageRenderer', $pageRender->reveal());
+
+        $viewHelper->_call('render');
+    }
+
+    protected function ajaxEnabledWorksDataProvider()
+    {
+        return array(
+            'nothing enabled' => array(
+                array('0', false, false)
+            )
+        );
+    }
+}
