d0aa2d7f6c5181e587a0f009a35172d13ba8698a
Napora <napora@drblitz-weblab.com>
2022-02-08 22:00:19 +0100

    [TASK] Update for TYPO3 11 LTS compatibility
    Update ext_emconf.php
    
    FIx typo
    
    Revert
    
    Fix typo

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'g.ringer@supseven.at',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '3.2.0',
+    'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99'
+            'typo3' => '11.0.99-11.9.99'
         ],
         'conflicts' => [],
         'suggests' => [
