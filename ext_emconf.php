6074fa2d1fd5335f55934a3fbcb21f0e1ef04067
Georg Ringer <georg.ringer@gmail.com>
2023-06-30 20:05:24 +0200

    [BUGFIX] Fix minimum t3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.99-12.4.99',
+            'typo3' => '11.5.28-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
